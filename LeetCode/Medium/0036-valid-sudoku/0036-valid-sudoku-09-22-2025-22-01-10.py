87ed77f5f9a30f274019485274441d6ceb7cde00
John Blanco <164598831+Leap-Day@users.noreply.github.com>
2025-09-22 22:01:11 -0400

    Array Medium 0 ms (100%) 17.8 MB (69.67%) .py  09-22-2025

---
+++ LeetCode/Medium/0036-valid-sudoku/0036-valid-sudoku-09-22-2025-22-01-10.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = defaultdict(set)
+        col = defaultdict(set)
+        box = defaultdict(set)
+
+        for rows in range(9):
+            for cols in range(9):
+                val = board[rows][cols]
+                
+                if val == '.':
+                    continue
+                boxindex = (rows // 3) * 3 + (cols // 3)
+                
+                if val in row[rows] or val in col[cols] or val in box[boxindex]:
+                    return False
+
+                row[rows].add(val)
+                col[cols].add(val)
+                box[boxindex].add(val)
+        
+        return True
