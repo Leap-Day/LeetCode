d9da05f215c33e3491c21e69864b5b2776c01a49
John Blanco <164598831+Leap-Day@users.noreply.github.com>
2025-09-26 14:39:34 -0400

    Array Medium 1014 ms (15.2%) 20.1 MB (97.21%) .py 09-26-2025

---
+++ LeetCode/Medium/0015-3sum/0015-3sum-09-26-2025-14-39-34.py
@@ -0,0 +1,32 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        l = 0
+        r = 0
+
+
+        for i in range(len(nums)):
+            # Skip any start numbers that we've already done
+            if i > 0 and nums[i - 1] == nums[i]:
+                continue
+            l = i + 1
+            r = len(nums) - 1
+            # Search for pairs in the rest of the array
+            while l < r:
+                if nums[i] + nums[l] + nums[r] == 0:
+                    res.append((nums[i], nums[l], nums[r]))
+                    l += 1
+                    r -= 1
+                    while nums[l] == nums[l - 1] and l < r: l += 1 # Skip repreat left numbers that came before
+                    while nums[r] == nums[r + 1] and l < r: r -= 1 # Skip repeat right numbers that came before
+                elif -nums[i] < nums[l] + nums[r]: # -(-1) < -1 + 4 Our right number is too big so we reduce 
+                    r -= 1
+                elif -nums[i] > nums[l] + nums[r]: # Do the opposite of above 
+                    l += 1
+                # We moved our pointers so let our loop go 
+        return res
+
+            
+            
+                
